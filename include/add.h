59c8f1d05f0559c705d0f3a2cf6f2c2badc90c16
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2024-11-06 18:33:27 +0900

    [wip]  this does not work.
    add.c.o does not generated.

---
+++ include/add.h
@@ -0,0 +1,14 @@
+#ifndef ADD_H
+#define ADD_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int add(int x, int y);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  // ADD_H
--- test/hello_test.cpp
+++ test/hello_test.cpp
@@ -1,7 +1,7 @@
+#include <add.h>
 #include <gtest/gtest.h>
 
-TEST(HelloTest, BasicAssertions)
-{
-    EXPECT_STRNE("hello", "world");
-    EXPECT_EQ(7 * 6, 42);
+TEST(HelloTest, BasicAssertions) {
+  EXPECT_STRNE("hello", "world");
+  EXPECT_EQ(7 * 6, 42);
 }
